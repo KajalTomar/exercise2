0e1894fd82f0643d4f59a39ad00d1c7a100eb2fc
Kajal Tomar <tomark@mouse.cs.umanitoba.ca>
2020-11-20 21:15:22 -0600

    insertItem and removeItem are testing

--- Makefile
+++ Makefile
@@ -1,8 +1,3 @@
-main: set.h set.c main.c
+main: set.h set.o main.c
 	clang -Wall main.c set.o -o main
 
-set.o: set.h set.c
-	clang -Wall -c set.c -o set.o
-
-clean:
-	rm set.o main.o main
--- main.c
+++ main.c
@@ -23,13 +23,15 @@ int testsExecuted = 0;
 //-----------------------------------------------------------------------------
 
 void insertCases(void);
+void removeCases(void);
 
 void testInsertItem(Set *, int, Boolean);
+void testRemoveItem(Set *, int, Boolean);
 
 int main(void)
 {
 	insertCases();
-
+	removeCases();
 	printf("end of processing.");
 	return(0);
 
@@ -79,43 +81,42 @@ void insertCases(void)
 // -----------------------------------------------------------------------------
 void removeCases(void)
 {
-	
+	Set * testSet = newSet();
+	int i; // to iterate
+	int totalSets = 5;
+
 	printf("----------------------------------------------------------------------------------------------------------\n");
 	printf("TESTS FOR removeItem()\n");
 	// test and print out the progress from the typical cases
 	printf("Testing typical cases.\n\n");
-	display();
+	
+	for(i = 0; i < totalSets; i++)
+	{
+		insertItem(testSet, i*totalSets); // [0 5 10 15 20 25]
+	}
 
-	testRemoveItem(5, true);
-	display();
-	testRemoveItem(12, true);
-	display();
-	testRemoveItem(-5, true);
-	display();
+	testRemoveItem(testSet, 5, true);
+	testRemoveItem(testSet, 15, true);
+	testRemoveItem(testSet, 10, true);
 	
 	printf("Removing an item that was not in the table...\n");
-	testRemoveItem(100, false); // item not in the table
-	display();
-	
+	testRemoveItem(testSet, 100, false); // item not in the table
+
 	printf("---------------------------\n");
 	printf("Testing edge cases.\n\n");
 	// test and print out the progress from the edge cases
 	
 	printf("Removing the entry in the first position of the table...\n");
-	testRemoveItem(2147483647, true); 
-	display();
+	testRemoveItem(testSet, 0, true); 
 	
 	printf("Removing the entry in the last position of the table...\n");
-	testRemoveItem(-2147483648, true); 
-	display();
+	testRemoveItem(testSet, 25, true); 
 	
 	printf("Removing the last entry left in the table...\n");
-	testRemoveItem(0, true); 
-	display();
-	
+	testRemoveItem(testSet, 20, true); 
+
 	printf("Removing an item when the table is empty...\n");
-	testRemoveItem(12, false); 
-	display();
+	testRemoveItem(testSet, 12, false); 
 	
 	printf("----------------------------------------------------------------------------------------------------------\n\n");
 
@@ -131,22 +132,22 @@ void testInsertItem(Set* test, int item, Boolean expectedResult)
 	{
 		if(testResult)
 		{
-			printf("Passed! %i is a valid entry that was inserted successfully in the table.\n", item);
+			printf("Passed! %i is a valid entry that was inserted successfully in the table.\n\n", item);
 		}
 		else
 		{
-			printf("Passed! %i is a *not* a valid (not unique) entry so it was *not* inserted in the table.\n", item);
+			printf("Passed! %i is a *not* a valid (not unique) entry so it was *not* inserted in the table.\n\n", item);
 		}
 	}
 	else 
 	{
 		if (testResult && !expectedResult)
 		{
-			printf("FAILED: I thought that the value would *not* be added to the table but it was added.\n");
+			printf("FAILED: I thought that the value would *not* be added to the table but it was added.\n\n");
 		} 
 		else 
 		{
-			printf("FAILED: I thought that the value would be added to the table but it was *not* added.\n");
+			printf("FAILED: I thought that the value would be added to the table but it was *not* added.\n\n");
 		}
 		testsFailed++;
 	}
@@ -162,10 +163,10 @@ void testInsertItem(Set* test, int item, Boolean expectedResult)
 // INPUT: int item is the item to be removed, the expected reults (bool) to
 // test against
 // -----------------------------------------------------------------------------
-void testRemoveItem(int item, bool expectedResult)
+void testRemoveItem(Set * testSet, int item, Boolean expectedResult)
 {
 
-	bool testResult = removeItem(item);
+	Boolean testResult = removeItem(testSet, item);
 
 	printf("Removing %i...\n", item);
 	
@@ -173,22 +174,22 @@ void testRemoveItem(int item, bool expectedResult)
 	{
 		if(testResult)
 		{
-			printf("Passed! %i was successfully removed from the table.\n", item);
+			printf("Passed! %i was successfully removed from the table.\n\n", item);
 		}
 		else
 		{
-			printf("Passed! %i was not removed from the table (and we didn't expect it to).\n", item);
+			printf("Passed! %i was not removed from the table (and we didn't expect it to).\n\n", item);
 		}
 	}
 	else 
 	{
 		if(testResult && !expectedResult)
 		{
-			printf("FAILED: I thought the %i would *not* be removed, but something was removed...\n", item);		
+			printf("FAILED: I thought the %i would *not* be removed, but something was removed...\n\n", item);		
 		}
 		else
 		{
-			printf("FAILED: I thought the %i would be removed, but it was *not*\n", item);		
+			printf("FAILED: I thought the %i would be removed, but it was *not*\n\n", item);		
 		}
 		testsFailed++;
 	}
